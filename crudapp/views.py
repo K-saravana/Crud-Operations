83b3e4db686d2b056c0c8ba09ce770e66746969f
Saravanan k <149131154+K-saravana@users.noreply.github.com>
2025-03-01 22:11:47 +0530

    changes

--- crudapp/views.py
+++ crudapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Member
 
 # Create your views here.
+# this is for testing 
 def index(request):
+
     mem=Member.objects.all()
     return render(request,'index.html',{'mem':mem})
